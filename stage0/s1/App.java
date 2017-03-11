bc059bcb2452b6361c9b49ae8aafc6db35c28118
Vsevolod Prokhortsev <seva.tver@ya.ru>
2017-03-11 23:54:07 +0300

    Add s2

--- stage0/s1/App.java
+++ stage0/s1/App.java
@@ -7,26 +7,26 @@ public class App {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         Integer max, min;
-        ArrayList<Integer> arrayListInt = new ArrayList(8);
+        ArrayList<Integer> arrayListInt = new ArrayList();
         System.out.println("Введите элементы массива, после нажмите ctrl+D");
         while (scan.hasNextInt()) {
             arrayListInt.add(scan.nextInt());
         }
 
-        Integer[] arrayInt = arrayListInt.toArray(new Integer[arrayListInt.size()]);
-        max = arrayInt[0];
-        min = arrayInt[0];
-        for (int i = 1; i < arrayInt.length; i++) {
-            if (max < arrayInt[i]) {
-                max = arrayInt[i];
+//        Integer[] arrayInt = arrayListInt.toArray(new Integer[arrayListInt.size()]);
+        max = arrayListInt.get(0);
+        min = arrayListInt.get(0);
+        for (int i = 1; i < arrayListInt.size(); i++) {
+            if (max < arrayListInt.get(i)) {
+                max = arrayListInt.get(i);
             }
-            if (min > arrayInt[i]){
-                min = arrayInt[i];
+            if (min > arrayListInt.get(i)){
+                min = arrayListInt.get(i);
             }
         }
 
-        for (int i = 0; i < arrayInt.length; i++) {
-            System.out.print(arrayInt[i] + " ");
+        for (int i = 0; i < arrayListInt.size(); i++) {
+            System.out.print(arrayListInt.get(i) + " ");
         }
         System.out.println("");
         System.out.println("max: " + max);
--- stage0/s2/App.java
+++ stage0/s2/App.java
@@ -1,23 +1,27 @@
-//package s2;
-//import java.util.ArrayList;
-//import java.util.Scanner;
-//
-///**
-// * Created by Seva_ on 11.03.2017.
-// */
-//public class App {
-//    public static void main(String[] args) {
-////        Scanner scan = new Scanner(System.in);
-////        String str;
-////        str = scan.nextLine();
-////        System.out.println(str);
-//
-//
-//        ArrayList a = new ArrayList();
-//        a.add(1);
-//        a.add(0);
-//        a.add(3);
-//        a.add(2);
-//        System.out.println(a);
-//    }
-//}
+package s2;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ * Created by Seva_ on 11.03.2017.
+ */
+public class App {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+//        ArrayList<String> arrayListInt = new ArrayList(8);
+        String str = "";
+        System.out.println("Введите элементы массива, после нажмите ctrl+D");
+        if (scan.hasNextLine()) {
+            str = scan.nextLine();
+        }
+
+//        String[] arrayString = arrayListInt.toArray(new String[arrayListInt.size()]);
+
+//        for (int i = 0; i < str.length; i++) {
+//            System.out.print( str[i] + " ");
+//        }
+
+        System.out.println(str);
+
+    }
+}
