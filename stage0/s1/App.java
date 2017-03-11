33b142ee0b3398ea5b6faae3ab30e2d6115a4e84
Vsevolod Prokhortsev <seva.tver@ya.ru>
2017-03-11 22:34:22 +0300

    Add s2

--- stage0/s1/App.java
+++ stage0/s1/App.java
@@ -5,24 +5,30 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int arrayLength = 0;
+//        int arrayLength = 0;
         int max, min;
-        System.out.println("Введите кол-во элементов массива");
+//        System.out.println("Введите кол-во элементов массива");
+//
+//        if (scan.hasNextInt()) {
+//            arrayLength = scan.nextInt();
+//        }
+//        int[] dva = new int[arrayLength];
+//
+//        for (int i = 0; i < arrayLength; i++) {
+//            System.out.println("Введите элемент массива №" + i + ":");
+//            if (scan.hasNextInt()) {
+//                dva[i] = scan.nextInt();
+//            }
+//        }
 
-        if (scan.hasNextInt()) {
-            arrayLength = scan.nextInt();
+//        max = dva[0];
+//        min = dva[0];
+        int[] dva = new int[4];
+        int i = 0;
+        while (scan.hasNextInt()) {
+
+
         }
-        int[] dva = new int[arrayLength];
-
-        for (int i = 0; i < arrayLength; i++) {
-            System.out.println("Введите элемент массива №" + i + ":");
-            if (scan.hasNextInt()) {
-                dva[i] = scan.nextInt();
-            }
-        }
-
-        max = dva[0];
-        min = dva[0];
 
         for (int i = 1; i < arrayLength; i++) {
             if (max < dva[i]) {
